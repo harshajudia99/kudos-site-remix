47b52e69a00972df1abcaa3db36ee2203cd7ae8a
harshajudia99 <145958537+harshajudia99@users.noreply.github.com>
2023-11-06 15:45:27 +0530

    user can add todo

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,22 +4,81 @@ import { Layout } from '~/components/layout'
 import { UserPanel } from '~/components/user-panel'
 import { getOtherUsers } from '~/utils/user.server'
 import { Outlet, useLoaderData } from '@remix-run/react'
+import { getFilteredKudos, getRecentKudos } from '~/utils/kudo.server'
+import { Kudo } from '~/components/kudo'
+import { Kudo as IKudo, Prisma, Profile } from '@prisma/client'
+import { RecentBar } from '~/components/recent-bar'
+import { SearchBar } from '~/components/serach-bar'
 
 export const loader: LoaderFunction = async ({ request }) => {
-    const userId = await requireUserId(request)
-    const users = await getOtherUsers(userId)
-    return json({ users }) 
+  const userId = await requireUserId(request)
+  const users = await getOtherUsers(userId)
+  const url = new URL(request.url)
+  const sort = url.searchParams.get('sort')
+  const filter = url.searchParams.get('filter')
+  const recentKudos = await getRecentKudos()
+
+  let sortOptions: Prisma.KudoOrderByWithRelationInput = {}
+  if (sort) {
+    if (sort === 'date') {
+      sortOptions = { createdAt: 'desc' }
+    }
+    if (sort === 'sender') {
+      sortOptions = { author: { profile: { firstName: 'asc' } } }
+    }
+    if (sort === 'emoji') {
+      sortOptions = { style: { emoji: 'asc' } }
+    }
+  }
+
+  let textFilter: Prisma.KudoWhereInput = {}
+  if (filter) {
+    textFilter = {
+      OR: [
+        { message: { mode: 'insensitive', contains: filter } },
+        {
+          author: {
+            OR: [
+              { profile: { is: { firstName: { mode: 'insensitive', contains: filter } } } },
+              { profile: { is: { lastName: { mode: 'insensitive', contains: filter } } } },
+            ],
+          },
+        },
+      ],
+    }
+  }
+
+  const kudos = await getFilteredKudos(userId, sortOptions, textFilter)
+
+  return json({ users, kudos, recentKudos })
 }
 
-export default function Home() {
+interface KudoWithProfile extends IKudo {
+  author: {
+    profile: Profile
+  }
+}
 
-    const { users } = useLoaderData()
+
+export default function Home() {
+  const { users, kudos, recentKudos } = useLoaderData()
 
   return (
     <Layout>
-        <Outlet/>
+      <Outlet />
       <div className="h-full flex">
-        <UserPanel users={users}/>
+        <UserPanel users={users} />
+        <div className="flex-1 flex flex-col">
+          <SearchBar />
+          <div className="flex-1 flex">
+            <div className="w-full p-10 flex flex-col gap-y-4">
+              {kudos.map((kudo: KudoWithProfile) => (
+                <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
+              ))}
+            </div>
+            <RecentBar kudos={recentKudos} />
+          </div>
+        </div>
       </div>
     </Layout>
   )
